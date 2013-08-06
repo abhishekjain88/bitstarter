18cd96d0c0896ba4729dcd995e3edda6e8420bf1
Abhishek Jain <abhi111jain@gmail.com>
2013-08-06 04:43:19 +0000

    Fixed a bug in web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
-  response.send(buf.toString("utf-8");
+  response.send(buf.toString("utf-8"));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
